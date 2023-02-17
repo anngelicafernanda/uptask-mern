10c9f2921bbe3fa4a8594fc321b01f5918c6b037
Angelica Contreras <afernandac94@gmail.com>
2023-02-17 18:35:18 -0300

    Ocultando las variables de entorno

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose';
 
 const conectarDB = async () => {
 	try {
-		const connection = await mongoose.connect(
-			'mongodb+srv://root:root1357%40@cluster0.lnfcpn3.mongodb.net/test',
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			},
-		);
+		const connection = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		const url = `${connection.connection.host}:${connection.connection.port}`;
 		console.log(`MongoDB Conectado en: ${url}`);
 	} catch (error) {
