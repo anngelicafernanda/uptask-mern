788618f015e94feb6438b66eff29f6288f2918de
Angelica Contreras <afernandac94@gmail.com>
2023-02-24 13:09:09 -0300

    Comprobando usuarios

--- backend/controllers/usuarioControllers.js
+++ backend/controllers/usuarioControllers.js
@@ -38,6 +38,17 @@ const autenticar = async (req, res) => {
 		return res.status(404).json({ msg: error.message });
 	}
 	//Comprobar su password
+
+	if (await usuario.comprobarPassword(password)) {
+		res.json({
+			_id: usuario._id,
+			nombre: usuario.nombre,
+			email: usuario.email,
+		});
+	} else {
+		const error = new Error('El password es Incorrecto');
+		return res.status(403).json({ msg: error.message });
+	}
 };
 
 export { registrar, autenticar };
