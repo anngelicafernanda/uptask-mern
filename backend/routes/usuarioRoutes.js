576301b05bf75ab30739579bc96d547dd5ce7a99
Angelica Contreras <afernandac94@gmail.com>
2023-02-27 13:41:55 -0300

    Creando Endpoint para confirmar cuentas

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { registrar, autenticar } from '../controllers/usuarioControllers.js';
+import {
+	registrar,
+	autenticar,
+	confirmar,
+} from '../controllers/usuarioControllers.js';
 
 //creacion, registro y confirmacion de usuarios
 router.post('/', registrar);
 router.post('/login', autenticar);
+router.get('/confirmar/:token', confirmar);
 
 export default router;
