0dbf8826ba7b62a777fbbdb497706cfc0168bb10
Angelica Contreras <afernandac94@gmail.com>
2023-04-20 16:30:42 -0400

    Obtener una Tarea y Validación

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -20,11 +20,24 @@ const agregarTarea = async (req, res) => {
 	} catch (error) {
 		console.log('🚀  error:', error);
 	}
-
-	console.log(existeProyecto);
 };
 
-const obtenerTarea = async (req, res) => {};
+const obtenerTarea = async (req, res) => {
+	const { id } = req.params || req.body;
+
+	const tarea = await Tarea.findById(id).populate('proyecto');
+
+	if (!tarea) {
+		const error = new Error('Tarea no encontrada');
+		return res.status(404).json({ msg: error.message });
+	}
+
+	if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+		const error = new Error('Acción no válida');
+		return res.status(403).json({ msg: error.message });
+	}
+	res.json(tarea);
+};
 
 const actualizarTarea = async (req, res) => {};
 
--- backend/routes/tareaRoutes.js
+++ backend/routes/tareaRoutes.js
@@ -15,10 +15,10 @@ router.post('/', checkAuth, agregarTarea);
 
 router
 	.route('/:id')
-	.get(obtenerTarea)
-	.put(actualizarTarea)
-	.delete(eliminarTarea);
+	.get(checkAuth, obtenerTarea)
+	.put(checkAuth, actualizarTarea)
+	.delete(checkAuth, eliminarTarea);
 
-router.post('/estado/:id', cambiarEstado);
+router.post('/estado/:id', checkAuth, cambiarEstado);
 
 export default router;
