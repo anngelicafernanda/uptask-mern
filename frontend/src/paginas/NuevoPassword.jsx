197d60e537c8060b49ca76ed0f74cd116fc39abe
Angelica Contreras <afernandac94@gmail.com>
2023-07-03 17:58:39 -0400

    Creando la Página de Nuevo Password y Confirmar Cuenta

--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,5 +1,33 @@
 const NuevoPassword = () => {
-	return <div>NuevoPassword</div>;
+	return (
+		<>
+			<h1 className="text-sky-600 font-black text-6xl capitalize">
+				Restablece tu password y no pierdas acceso a tus{' '}
+				<span className="text-slate-700">Proyectos</span>
+			</h1>
+			<form className="my-10 bg-white shadow rounded-lg p-10" action="">
+				<div className="my-5">
+					<label
+						className="uppercase text-gray-600 block text-xl font-bold"
+						htmlFor="password"
+					>
+						Nuevo Password
+					</label>
+					<input
+						className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+						id="password"
+						type="password"
+						placeholder="Escribe tu nuevo Password"
+					/>
+				</div>
+				<input
+					type="submit"
+					value="Guardar Nuevo Password"
+					className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-colors"
+				/>
+			</form>
+		</>
+	);
 };
 
 export default NuevoPassword;
