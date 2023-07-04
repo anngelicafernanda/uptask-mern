bbdd630cc8d99c30086cdbb8ca1d3f200784b6a6
Angelica Contreras <afernandac94@gmail.com>
2023-07-04 19:59:15 -0400

    Validando el Formulario

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import Alerta from '../components/Alerta';
+
 const Registrar = () => {
+	const [nombre, setNombre] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [repetirPassword, setRepetirPassword] = useState('');
+	const [alerta, setAlerta] = useState({});
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if ([nombre, email, password, repetirPassword].includes('')) {
+			setAlerta({
+				msg: 'Todos los campos son obligatorios',
+				error: true,
+			});
+			return;
+		}
+	};
+
+	const { msg } = alerta;
+
 	return (
 		<>
 			<h1 className="text-sky-600 font-black text-6xl capitalize">
 				Crea tu Cuenta y Administra Tus{' '}
 				<span className="text-slate-700">Proyectos</span>
 			</h1>
-			<form className="my-10 bg-white shadow rounded-lg p-10" action="">
+			{msg && <Alerta alerta={alerta} />}
+			<form
+				onSubmit={handleSubmit}
+				className="my-10 bg-white shadow rounded-lg p-10"
+				action=""
+			>
 				<div className="my-5">
 					<label
 						className="uppercase text-gray-600 block text-xl font-bold"
@@ -20,6 +47,8 @@ const Registrar = () => {
 						id="nombre"
 						type="text"
 						placeholder="Escribe tu Nombre"
+						value={nombre}
+						onChange={(e) => setNombre(e.target.value)}
 					/>
 				</div>
 				<div className="my-5">
@@ -34,6 +63,8 @@ const Registrar = () => {
 						id="email"
 						type="email"
 						placeholder="Email de Registro"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
 					/>
 				</div>
 				<div className="my-5">
@@ -48,6 +79,8 @@ const Registrar = () => {
 						id="password"
 						type="password"
 						placeholder="Password de Registro"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 				</div>
 				<div className="my-5">
@@ -62,6 +95,8 @@ const Registrar = () => {
 						id="repetirPassword"
 						type="password"
 						placeholder="Repetir Tu Password"
+						value={repetirPassword}
+						onChange={(e) => setRepetirPassword(e.target.value)}
 					/>
 				</div>
 				<input
