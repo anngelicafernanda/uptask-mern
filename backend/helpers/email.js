c563d6f7dffd4fccfd44b691f89cdd01883607d9
Angelica Contreras <afernandac94@gmail.com>
2023-07-05 17:56:53 -0400

    Enviando el Email de Bienvenida

--- backend/helpers/email.js
+++ backend/helpers/email.js
@@ -1,5 +1,28 @@
-import nodeMailer from 'nodemailer';
+import nodemailer from 'nodemailer';
 
-export const emailRegistro = (datos) => {
-	console.log('🚀  datos:', datos);
+export const emailRegistro = async (datos) => {
+	const { email, nombre, token } = datos;
+
+	var transport = nodemailer.createTransport({
+		host: 'sandbox.smtp.mailtrap.io',
+		port: 2525,
+		auth: {
+			user: '8b5ceae61fe95b',
+			pass: '41d1ab6ddfc51a',
+		},
+	});
+
+	// informacion del email
+
+	const info = await transport.sendMail({
+		from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com>',
+		to: email,
+		subject: 'UpTask - Comprueba tu cuenta',
+		text: 'Comprueba tu cuenta en UpTask',
+		html: `<p>Hola: ${nombre} Comprueba tu cuenta en UpTask</p>
+    <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace:</p>
+    <a href=${process.env.FRONTEND_URL}/confirmar/${token}>Comprobar cuenta</a>
+    <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+    `,
+	});
 };
