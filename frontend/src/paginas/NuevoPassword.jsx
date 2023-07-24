68461df8ed8c8e1dae1c4f92917b730fb023fa33
Angelica Contreras <afernandac94@gmail.com>
2023-07-24 12:36:11 -0400

    Almacenando un nuevo password

--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -4,8 +4,10 @@ import axios from 'axios';
 import Alerta from '../components/Alerta';
 
 const NuevoPassword = () => {
+	const [password, setPassword] = useState('');
 	const [tokenValido, setTokenValido] = useState(false);
 	const [alerta, setAlerta] = useState({});
+	const [passwordModificado, setPasswordModificado] = useState(false);
 	const params = useParams();
 	const { token } = params;
 
@@ -27,6 +29,31 @@ const NuevoPassword = () => {
 		comprobarToken();
 	}, []);
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (password.length < 6) {
+			setAlerta({
+				msg: 'El password debe ser mínimo de 6 carácteres.',
+				error: true,
+			});
+			return;
+		}
+		try {
+			const url = `http://localhost:4000/api/usuarios/olvide-password/${token}`;
+			const { data } = await axios.post(url, { password });
+			setAlerta({
+				msg: data.msg,
+				error: false,
+			});
+			setPasswordModificado(true);
+		} catch (error) {
+			setAlerta({
+				msg: error.response.data.msg,
+				error: true,
+			});
+		}
+	};
+
 	const { msg } = alerta;
 
 	return (
@@ -37,7 +64,10 @@ const NuevoPassword = () => {
 			</h1>
 			{msg && <Alerta alerta={alerta} />}
 			{tokenValido && (
-				<form className="my-10 bg-white shadow rounded-lg p-10" action="">
+				<form
+					className="my-10 bg-white shadow rounded-lg p-10"
+					onSubmit={handleSubmit}
+				>
 					<div className="my-5">
 						<label
 							className="uppercase text-gray-600 block text-xl font-bold"
@@ -50,6 +80,8 @@ const NuevoPassword = () => {
 							id="password"
 							type="password"
 							placeholder="Escribe tu nuevo Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
 					<input
@@ -58,6 +90,14 @@ const NuevoPassword = () => {
 						className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-colors"
 					/>
 				</form>
+			)}
+			{passwordModificado && (
+				<Link
+					className="block text-center my-5 text-slate-500 uppercase text-sm"
+					to="/"
+				>
+					Inicia Sesión
+				</Link>
 			)}
 		</>
 	);
